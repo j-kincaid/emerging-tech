9e850555e1a3b426934715f11bbdeb72cfd348a5
Jessica Kincaid <4j.kincaid@gmail.com>
2024-12-09 16:24:53 -0600

    Add script for carousel

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// Hype Accordion 
+
+$(document).ready(function(){
+  $('#navigation ul > li ul')
+    .click(function(event){
+      event.stopPropagation();
+    })
+    .filter(':not(:first)')
+    .hide();
+    
+  $('#navigation ul > li').click(function(){
+    var selfClick = $(this).find('ul:first').is(':visible');
+    if(!selfClick) {
+      $(this)
+        .parent()
+        .find('> li ul:visible')
+        .slideToggle();
+    }
+    
+    $(this)
+      .find('ul:first')
+      .stop(true, true)
+      .slideToggle();
+  });
+});
+
+
+// "Scroll to top" button
+
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+      $('#toTop').fadeIn();
+  } else {
+      $('#toTop').fadeOut();
+  }
+});
+
+$("#toTop").click(function() {
+  $("html, body").animate({scrollTop: 0}, 500);
+});
+
+$(document).ready(function(){
+  $('.autoplay').slick({
+    autoplay: true
+  });
+});
+
+// Slick carousel initialization function
+
+$(document).ready(function(){
+  $('.autoplay').slick({
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    mobileFirst: true
+  });
+});
+
+		
